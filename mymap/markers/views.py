f256c936b1fe44330b290c421bec447a4eb096d7
silveiratcl <silveira.tcl@gmail.com>
2022-02-25 17:31:40 -0300

    first commit

---
+++ mymap/markers/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+"""Markers view."""
+
+from django.views.generic.base import TemplateView
+
+
+class MarkersMapView(TemplateView):
+    """Markers map view."""
+
+    template_name = "map.html"
